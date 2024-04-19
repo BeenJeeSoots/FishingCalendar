7f5148a465be7f9523b3a2adf6c1d99039b8f48f
BeenJee <bdsutton@mtu.edu>
2024-04-19 17:41:59 -0400

    Read/Write event functionality
    Added read/write capabilities to the Event file.
    
    added fish.
    
    added new ui.
    
    I just wanna graduate.

--- My project/Assets/Event.cs
+++ My project/Assets/Event.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 
 public class Event : CalendarItem {
 
@@ -19,5 +22,54 @@ public class Event : CalendarItem {
     public Event CreateEvent(string title, string description, string location, string startTime, string endTime){
         return new Event( title,  description,  location,  startTime,  endTime);
     }
+
+    public void SaveEvent()
+    {
+        string filePath = "eventsDatabase.txt";
+        try
+        {
+            using StreamWriter writer = new StreamWriter(filePath, true);
+            writer.WriteLine($"Title: {title}");
+            writer.WriteLine($"Description: {description}");
+            writer.WriteLine($"Location: {Location}");
+            writer.WriteLine($"Start Time: {StartTime}");
+            writer.WriteLine($"End Time: {EndTime}");
+            writer.WriteLine();
+        }
+        catch (Exception e)
+        {
+            Debug.WriteLine($"Error saving event to file: {e.Message}");
+        }
+    }
+
+
+    public Event[] LoadEvents()
+    {
+        Event[] events = new Event[100];
+        string filePath = "eventsDatabase.txt";
+        try
+        {
+            // here is where we would iterate through the list in the database and obtain all of them
+            // Perhaps enumerate each event with an index. 
+            using StreamReader reader = new StreamReader(filePath);
+            string title = reader.ReadLine()?.Split(':')[1].Trim();
+            string description = reader.ReadLine()?.Split(':')[1].Trim();
+            string location = reader.ReadLine()?.Split(':')[1].Trim();
+            string startTime = reader.ReadLine()?.Split(':')[1].Trim();
+            string endTime = reader.ReadLine()?.Split(':')[1].Trim();
+
+            events.Append(new Event(title, description, location, startTime, endTime));
+        }
+        catch (Exception e)
+        {
+            Debug.WriteLine($"Error loading event from file: {e.Message}");
+            return null;
+        }
+        return events;
+    }
+
+
+
+
 
 }
